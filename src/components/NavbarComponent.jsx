230b8161342c57a6149a0fd91c00e66e6320c9a5
“andreeffendi” <andreffendii@tanyadok.com>
2021-08-19 13:12:09 +0700

    upload data ke github

---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+  Container,
+} from "reactstrap";
+
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand href="/">Home</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/"></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://andreeffendi.github.io">
+                  About Me
+                </NavLink>
+              </NavItem>
+             
+            </Nav>
+            <NavbarText>Admin</NavbarText>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
